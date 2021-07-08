5a2deb040b082353a4527e6290d77344ad112d83
sarifou <m.diallosarifou@gmail.com>
2021-07-08 16:46:39 +0200

    Header first design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProcessComponent } from './components/process/process.component';
+import { AutoComponent } from './components/auto/auto.component';
+import { ConsoleComponent } from './components/console/console.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    ProcessComponent,
+    AutoComponent,
+    ConsoleComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

92d5cc43b34589db474d06967376b0a6ce291d68
sarifou <m.diallosarifou@gmail.com>
2021-07-09 14:50:21 +0200

    Home and simulator first design

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,7 +7,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  process = [] as any ;
+  constructor() { 
+    this.process = [
+      {
+        "id" : 1002012,
+        "desc" : "Loading of the 120B container on to the agv 12 via the gantry 2",
+        "client" : "Cherif",
+        "state" : "In progress"
+      },
+      {
+        "id" : 1204521,
+        "desc" : "Loading of the 120B container on to the agv 12 via the gantry 2",
+        "client" : "Cherif",
+        "state" : "finish"
+      },
+      {
+        "id" : 1204522,
+        "desc" : "Loading of the 120B container on to the agv 12 via the gantry 2",
+        "client" : "Cherif",
+        "state" : "Interrupted"
+      },
+      {
+        "id" : 1204521,
+        "desc" : "Loading of the 120B container on to the agv 12 via the gantry 2",
+        "client" : "Cherif",
+        "state" : "finish"
+      },
+      {
+        "id" : 1204521,
+        "desc" : "Loading of the 120B container on to the agv 12 via the gantry 2",
+        "client" : "Cherif",
+        "state" : "finish"
+      },
+      {
+        "id" : 1204521,
+        "desc" : "Loading of the 120B container on to the agv 12 via the gantry 2",
+        "client" : "Cherif",
+        "state" : "finish"
+      },
+      {
+        "id" : 1204521,
+        "desc" : "Loading of the 120B container on to the agv 12 via the gantry 2",
+        "client" : "Cherif",
+        "state" : "finish"
+      },
+      {
+        "id" : 1204521,
+        "desc" : "Loading of the 120B container on to the agv 12 via the gantry 2",
+        "client" : "Cherif",
+        "state" : "finish"
+      },
+      {
+        "id" : 1204521,
+        "desc" : "Loading of the 120B container on to the agv 12 via the gantry 2",
+        "client" : "Cherif",
+        "state" : "finish"
+      },
+    ]
+  }
 
   ngOnInit(): void {
   }
